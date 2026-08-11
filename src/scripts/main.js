77445fae8b70e97fe64462a82124f86196ccb971
Luiz Henrique <luizmaximinoprofissional@gmail.com>
2026-08-10 21:47:34 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,5 @@
 //Criando uma data futura//
-const dataDoEvento = new Date("jul 12, 2024 19:00:00:00");
+const dataDoEvento = new Date("jul 12, 2028 19:00:00:00");
 
 //recuperar  o timeStamp do futuro//
 const timeStampDoEvento = dataDoEvento.getTime();
